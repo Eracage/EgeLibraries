223a2c9ede902a95aeaaf4705e31d5b489ee8dbd
Esko Haila <eracage@gmail.com>
2014-04-29 01:55:29 +0300

    reworked structure

--- EgeTest/VSProject/main.cpp
+++ EgeTest/VSProject/main.cpp
@@ -1,2 +1,8 @@
-#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.h"
+
+#include <EgeMath/Math.hpp>
+#include <Ege2D/World2D.hpp>
+
+int main()
+{
+	return 0;
+}
--- EgeTestCase/main.cpp
+++
@@ -1,8 +0,0 @@
-
-#include <EgeMath/Math.hpp>
-#include <Ege2D/World2D.hpp>
-
-int main()
-{
-	return 0;
-}
